72b6a61416f655f0ceb88620cbdf1e142a816e4a
Miguel Costero <miguel3490@gmail.com>
2022-04-04 14:39:00 -0300

    feat(backend): loading test data

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,15 +1,22 @@
 import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import logger from './logger';
+import loadTestData from './load-test-data';
 
 export const connectToDatabase = async () => {
     const mongoServer = await MongoMemoryServer.create();
     const mongoUri = mongoServer.getUri();
 
     const connection = await mongoose.connect(mongoUri);
-    connection.set('debug', true);
+    connection.set('debug', process.env.NODE_ENV !== 'production');
 
     logger.info('Connected to database');
+
+    if (process.env.NODE_ENV !== 'production') {
+        logger.info(`MongoDB URI: ${mongoUri}`);
+        logger.debug('loading test data...');
+        await loadTestData();
+    }
 
     return connection;
 };
---
+++ backend/src/load-test-data.ts
@@ -0,0 +1,25 @@
+import logger from './logger';
+import { User, UserModel } from './models/user.model';
+import { Types } from 'mongoose';
+
+const testUsers: any[] = [
+    {
+        _id: new Types.ObjectId('624b2c7606419272411e051f'),
+        email: 'miguel@test.com',
+        password:
+            'd17a8028673f78a4ae113a69c3b3babd6944eca6b29a2fe4044ef4e2e56eb7e6a0da92d7fdf3886b2f274ebfbf2e2cbb8b764909d0e110eb467f5b42c5cb3697',
+        salt: 'c17cfb8f242d37766c84dbff0ab139a3',
+        firstName: 'Miguel',
+        lastName: 'Costero',
+        createdAt: new Date('Mon, 04 Apr 2022 17:35:50 GMT'),
+        updatedAt: new Date('Mon, 04 Apr 2022 17:35:50 GMT'),
+        __v: 0,
+    },
+];
+
+const loadTestData = async () => {
+    logger.debug('Loading users...');
+    await Promise.all(testUsers.map((user) => UserModel.create(user)));
+};
+
+export default loadTestData;
