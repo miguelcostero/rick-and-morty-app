371d82fbdefd6320aca30846b78421e19e64ead2
Miguel Costero <miguel3490@gmail.com>
2022-04-04 20:33:09 -0300

    feat(frontend): styling spinner

--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -1,7 +1,51 @@
 import { FC } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const animation = keyframes`
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+`;
+
+const Loader = styled.div`
+    margin: 60px auto;
+    font-size: 5px;
+    position: relative;
+    text-indent: -9999em;
+    border-top: 1.1em solid rgba(255, 255, 255, 0.2);
+    border-right: 1.1em solid rgba(255, 255, 255, 0.2);
+    border-bottom: 1.1em solid rgba(255, 255, 255, 0.2);
+    border-left: 1.1em solid #ffffff;
+    transform: translateZ(0);
+    animation: ${animation} 1.1s infinite linear;
+
+    &,
+    &::after {
+        border-radius: 50%;
+        width: 3rem;
+        height: 3rem;
+    }
+`;
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    height: 100vh;
+`;
 
 const Spinner: FC = () => {
-    return <p>loading...</p>;
+    return (
+        <Container>
+            <Loader>Loading...</Loader>
+        </Container>
+    );
 };
 
 export default Spinner;
